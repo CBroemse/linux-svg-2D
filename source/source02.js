c3370e81365bd6b8c2232ddf76b170fca378cc75
Cee <35766903+CBroemse@users.noreply.github.com>
2022-10-01 23:46:26 +0200

    Add files via upload

---
+++ source/source02.js
@@ -0,0 +1,3 @@
+function changeFill() {
+    document.getElementById("hexagon").style.fill = "rgb(" + Math.floor(255 * Math.random()) + ", " + Math.floor(255 * Math.random()) + ", " + Math.floor(255 * Math.random()) + ")";
+}
